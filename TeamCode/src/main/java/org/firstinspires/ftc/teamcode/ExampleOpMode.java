eae8767b9efd3d1d68b63845d448084beeb69435
Chad Steinbaugh <csteinbaugh@ad.nsd.org>
2017-10-10 15:37:18 -0700

    Added ExampleOpMode as a systems test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleOpMode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by csteinbaugh on 10/10/2017.
+ */
+
+public class ExampleOpMode extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
