b5a8e14d68442356db596b23c46750651fe06a11
Chad Steinbaugh <csteinbaugh@ad.nsd.org>
2017-10-10 15:51:37 -0700

    Edited header comment, and added annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleOpMode.java
@@ -1,15 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /**
  * Created by csteinbaugh on 10/10/2017.
+ * Example OpMode to demonstrate the general order of commands in an OpMode
  */
 
-public class ExampleOpMode extends LinearOpMode
-{
+@TeleOp(name = "Example")
+public class ExampleOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        
+
     }
 }
