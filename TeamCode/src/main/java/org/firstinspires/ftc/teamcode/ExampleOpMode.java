b09dec9c97ec33bbc3773d42f7b6b42eca4ce128
AD\csteinbaugh <csteinbaugh@apps.nsd.org>
2017-10-24 15:43:20 -0700

    Filled out example OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleOpMode.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by csteinbaugh on 10/10/2017.
@@ -9,9 +11,31 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  */
 
 @TeleOp(name = "Example")
-public class ExampleOpMode extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
+public class ExampleOpMode extends LinearOpMode
+{
+    DcMotor motorL = null;
+    DcMotor motorR = null;
+    Servo servoFlipper = null;
 
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        InitRobot();
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            motorL.setPower(gamepad1.left_stick_y);
+            motorR.setPower(gamepad1.right_stick_y);
+        }
+
+    }
+
+    void InitRobot()
+    {
+        motorL = hardwareMap.get(DcMotor.class, "motorL");
+        motorR = hardwareMap.get(DcMotor.class, "motorR");
+        servoFlipper = hardwareMap.get(Servo.class, "servoFlipper");
     }
 }
